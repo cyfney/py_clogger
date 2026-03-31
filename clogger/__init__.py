12a5467ba60b44d119c7863429503168e03db24c
cyfney-anka <cyfney-anka@qq.com>
2026-03-31 18:31:17 +0800

    first commit

---
+++ clogger/__init__.py
@@ -0,0 +1,3 @@
+from .core import log_v, log_i, log_d, log_e, log_warning
+
+__all__ = ["log_v", "log_i", "log_d", "log_warning", "log_e"]
---
+++ examples/main.py
@@ -0,0 +1,12 @@
+from clogger import log_v, log_i, log_d, log_warning, log_e
+
+def main():
+    log_v("test")
+    log_i("test")
+    log_d("test")
+    log_warning("test")
+    log_e("test")
+
+
+if __name__ == "__main__":
+    main()
